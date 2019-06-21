8820a49877165dda05012859a8bcbceeb2573ff2
Andrew Zajac <ajzajac@icloud.com>
2019-06-21 19:18:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments){
   return array;
   }
   
-function johnLennonFacts() {
+function johnLennonFacts(facts) {
   const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0;
   while (i < facts.length) {
